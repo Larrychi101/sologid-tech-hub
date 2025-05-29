d1eea78bc222ceda0974ca04596366e02c08ab47
larrychi101 <lawchi20@yahoo.com>
2025-05-29 05:47:10 +0100

    Deploy to GitHub Pages

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ export const Hero = () => {
           transition={{ duration: 0.5 }}
         >
           <h3 className="text-secondaryColor text-sm sm:text-base  mb-6 sm:mt-32 mt-20  font-bold">
-            Transform Your Passion into 
+          Transform Your STEM Capabilities into
           </h3>
         </motion.div>
         <motion.div
@@ -27,8 +27,8 @@ export const Hero = () => {
           transition={{ duration: 0.5, delay: 0.05 }}
         >
           <div className="text-5xl sm:text-6xl lg:text-7xl xl:text-7xl font-bold tracking-wide  text-primaryText  px-8 sm:px-8 md:px-20 lg:px-4">
-            <h1 className="inline md:hidden">Intelligence</h1>
-            <h1 className="hidden md:inline">Intelligence of Things</h1>
+            <h1 className="inline md:hidden">AI & IoT-Powered</h1>
+            <h1 className="hidden md:inline">AI & IoT-Powered</h1>
           </div>
           <h1 className="mt-2 sm:mt-2 text-4xl sm:text-6xl lg:text-7xl xl:text-7xl font-bold tracking-wide  text-primaryText  px-8 sm:px-20 md:px-24 lg:px-24">
             Startup
@@ -40,7 +40,10 @@ export const Hero = () => {
           transition={{ duration: 0.5, delay: 0.1 }}
         >
           <h2 className="text-secondaryText text-sm lg:text-base xl:text-lg sm:text-base mt-10 px-12 sm:px-48 ">
-          We empower Africa-focused skilled professionals by leveraging identity overlay network protocols and Microsoft-powered digital twin technology to unlock new revenue generation models
+          We are a next-gen digital innovation hub empowering Africa-focused
+            STEM professionals and startups with smart technologies and tailored
+            resources to unlock new revenue streams and drive sustainable
+            growth
           </h2>
         </motion.div>
         <motion.div
